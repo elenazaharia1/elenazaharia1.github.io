9716c3f45270c22e5bad4555abfd8f0ed5817c1a
Elena <webdevelena30@gmail.com>
2023-01-05 19:30:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
 var skillsEl = document.getElementById("skills-list");
 
-// var oldText = skillsEl.innerHTML;
-
-// skillsEl.innerHTML = oldText + "<li>JS</li>";
-// skillsEl.innerHTML += '<li class="favourite">HTML</li>';
-// skillsEl.innerHTML += '<li class="favourite">CSS</li>';
-
 var skills = ["HTML", "CSS", "JS", "Drive"];
 
-// skillsEl.innerHTML += "<li>" + skills[0] + "</li>";
-// skillsEl.innerHTML += "<li>" + skills[1] + "</li>";
-// skillsEl.innerHTML += "<li>" + skills[2] + "</li>";
-
-// var skillsHTML = "";
-
-// skillsHTML += "<li>" + skills[0] + "</li>";
-// skillsHTML += "<li>" + skills[1] + "</li>";
-// skillsHTML += "<li>" + skills[2] + "</li>";
-
-// skillsEl.innerHTML = skillsHTML;
-
 var skillsHTML = "";
-var i = 0;
 
-// skillsHTML += "<li>" + skills[i] + "</li>";
-// i++;
-// skillsHTML += "<li>" + skills[i] + "</li>";
-// i++;
-// skillsHTML += "<li>" + skills[i] + "</li>";
-// i++;
-// skillsHTML += "<li>" + skills[i] + "</li>";
-// i++;
-
-for (var i = 0; i < skills.length; ) {
+for (var i = 0; i < skills.length; i++) {
   skillsHTML += "<li>" + skills[i] + "</li>";
-  i++;
 }
 
 skillsEl.innerHTML = skillsHTML;
